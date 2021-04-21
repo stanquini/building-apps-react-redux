a74f6c4dddea625762ba46abfc419f0e961f0c50
Alfredo Stanquini Neto <alfredostanquinineto@Alfredos-MacBook-Pro.local>
2021-04-21 16:47:14 -0300

    confirguracao redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from "react";
 import { render } from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css"
+import App from './components/App'
+import "./index.css"
+import configureStore from "./redux/configureStore";
+import { Provider as ReduxProvider } from 'react-redux'
 
-function Hi() {
-  return <p>Hi.</p>;
-}
+const store = configureStore();
 
-render(<Hi />, document.getElementById("app"));
+render(
+  <ReduxProvider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </ReduxProvider>
+  , document.getElementById("app"));
