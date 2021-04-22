49fb0eea1a5c3639e80a78f3db9456c8cfe65e7e
Alfredo Stanquini Neto <stanquini@gmail.com>
2021-04-22 17:35:18 -0300

    converte class para funcao componente com Hooks

---
+++ src/components/courses/ManageCoursePage.js
@@ -0,0 +1,51 @@
+import React, { useEffect } from "react";
+import { connect } from 'react-redux'
+import { loadCourses } from '../../redux/actions/courseActions'
+import { loadAuthors } from "../../redux/actions/authorActions";
+import PropTypes from 'prop-types'
+
+
+function ManageCoursePage({ courses, authors, loadAuthors, loadCourses }) {
+
+  useEffect(()=> {
+     if(courses.length === 0) {
+       loadCourses().catch(error => {
+         alert("Loading courses failed" + error);
+       })
+     }
+     if(authors.length === 0) {
+       loadAuthors().catch(error => {
+         alert("Loading authors failed" + error);
+       })
+     }
+  }, []  )
+
+  return (
+    <>
+      <h2>Manager Course</h2>
+    </>
+  )
+
+}
+
+ManageCoursePage.propTypes = {
+  authors: PropTypes.array.isRequired,
+  courses: PropTypes.array.isRequired,
+  loadCourses: PropTypes.func.isRequired,
+  loadAuthors: PropTypes.func.isRequired
+}
+
+function mapStateToProps(state) {
+  return {
+    courses: state.courses,
+    authors: state.authors
+  }
+}
+
+const mapDispatchToProps = {
+  loadCourses,
+  loadAuthors
+}
+
+const connectStateAndProps = connect(mapStateToProps, mapDispatchToProps)
+export default  connectStateAndProps(ManageCoursePage);
