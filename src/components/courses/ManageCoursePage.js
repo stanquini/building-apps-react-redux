7f2476fc2094192f50820398aabe4455b4fc7bb0
Alfredo Stanquini Neto <stanquini@gmail.com>
2021-04-24 18:42:20 -0300

    Form de Cursos no fluxo do Redux

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -1,11 +1,15 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from 'react-redux'
-import { loadCourses } from '../../redux/actions/courseActions'
+import { loadCourses, saveCourse } from '../../redux/actions/courseActions'
 import { loadAuthors } from "../../redux/actions/authorActions";
 import PropTypes from 'prop-types'
+import CourseForm from "./CourseForm";
+import { newCourse } from "../../../tools/mockData"
 
+function ManageCoursePage({ courses, authors, loadAuthors, loadCourses, saveCourse, ...props }) {
 
-function ManageCoursePage({ courses, authors, loadAuthors, loadCourses }) {
+  const [ course, setCource ] = useState({...course});
+  const [ errors, setErrors ] = useState({})
 
   useEffect(()=> {
      if(courses.length === 0) {
@@ -18,25 +22,46 @@ function ManageCoursePage({ courses, authors, loadAuthors, loadCourses }) {
          alert("Loading authors failed" + error);
        })
      }
-  }, []  )
+  }, []  );
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setCource(prevCourse => ({
+      ...prevCourse,
+      [name]: name === "authorId"? parseInt(value, 10): value
+    }))
+  }
+
+  function handleSave(event) {
+    event.preventDefault();
+    saveCourse(course);
+  }
 
   return (
     <>
-      <h2>Manager Course</h2>
+      <CourseForm
+        authors={authors}
+        errors={errors}
+        course={course}
+        onChange={handleChange}
+        onSave={handleSave}
+      />
     </>
   )
-
 }
 
 ManageCoursePage.propTypes = {
+  course: PropTypes.object.isRequired,
   authors: PropTypes.array.isRequired,
   courses: PropTypes.array.isRequired,
   loadCourses: PropTypes.func.isRequired,
-  loadAuthors: PropTypes.func.isRequired
+  loadAuthors: PropTypes.func.isRequired,
+  saveCourse: PropTypes.func.isRequired
 }
 
 function mapStateToProps(state) {
   return {
+    course: newCourse,
     courses: state.courses,
     authors: state.authors
   }
@@ -44,7 +69,8 @@ function mapStateToProps(state) {
 
 const mapDispatchToProps = {
   loadCourses,
-  loadAuthors
+  loadAuthors,
+  saveCourse
 }
 
 const connectStateAndProps = connect(mapStateToProps, mapDispatchToProps)
